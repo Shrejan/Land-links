4e0a68e287b75b45e176f31c7d86e32c56d706c6
Shrejan Kotyan <shrejankotyan@gmail.com>
2025-02-07 00:24:46 +0530

    Fixed CORS issue

--- server/index.js
+++ server/index.js
@@ -11,8 +11,23 @@ const port = 5000;
 
 
 
+const allowedOrigins = ['https://land-links.vercel.app'];
 
-app.use(cors({ origin: "*", methods: "GET,POST,PUT,DELETE" }));
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Max-Age', '86400'); // 24 hours
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
 
 app.use(express.json());
 
