81a63eeefd4592106babce11947f10fe85e4977a
Shrejan Kotyan <163548328+Shrejan@users.noreply.github.com>
2025-02-08 14:12:45 +0530

    Update db.js

--- server/api/routes/db.js
+++ server/api/routes/db.js
@@ -1,14 +1,1 @@
-import mongoose from'mongoose';
-import dotenv from 'dotenv';dotenv.config();
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected');
-  } catch (error) {
-    console.error('Connection error:', error.message);
-    process.exit(1);
-  }
-};
-
-export default connectDB;
