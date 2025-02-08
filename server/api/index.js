7b083539837b2a152338ecf6235665f08d43e02c
Shrejan Kotyan <shrejankotyan@gmail.com>
2025-02-08 22:43:39 +0530

    jai mahishmathi sambrajyam 5nd

--- server/api/index.js
+++ server/api/index.js
@@ -17,14 +17,14 @@ const allowedOrigins = ["https://land-links.vercel.app"];
 app.use(cors({
   origin: 'https://land-links.vercel.app',
   methods: ['POST', 'GET', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  //allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
 // Handle preflight requests
 app.options('*', cors());
 
-app.use(express.json({ limit: '20mb' }));
+app.use(express.json({ limit: '10mb' }));
 
     // Ensure the database is connected (only connects if not already connected)
  connectDB()
