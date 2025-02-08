b5492b29e8406bb9a31255dcbeccccf436475f0f
Shrejan Kotyan <163548328+Shrejan@users.noreply.github.com>
2025-02-08 11:41:00 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,9 @@
 import serverless from "serverless-http";
 import express from "express";
 import cors from "cors";
-import connectDB from "../db.js";
+import connectDB from "./db.js";
 import Post from "../models/Post.js";
-import Account_creation from "../routes/ac_creation.js";
+import Account_creation from "./routes/ac_creation.js";
 import User from "../models/Accounts.js";
 
 
