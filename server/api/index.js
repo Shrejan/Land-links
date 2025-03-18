a571f5ed9347546db4e7e04fc2b2d818bd164f42
Shrejan Kotyan <shrejankotyan@gmail.com>
2025-03-18 20:50:58 +0530

    will it work6

--- server/api/index.js
+++ server/api/index.js
@@ -9,7 +9,7 @@ import cors from "cors";
 
 const app = express();
 
-const allowedOrigins = ["https://land-links.vercel.app"];
+//const allowedOrigins = ["https://land-links.vercel.app"];
 
 // Configure CORS properly
 app.use(
@@ -55,9 +55,9 @@ app.post("/api/data", async(req, res) => {console.log('Running on Vercel:', proc
   }
 });
 
-const port = process.env.PORT || 5000;
+/*const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-});
+});*/
 
 export default serverless(app);
