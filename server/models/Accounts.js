44176d807e7bc99890e3426b80ee72b85785a1c6
Shrejan Kotyan <shrejankotyan@gmail.com>
2025-03-25 22:26:38 +0530

    now it is working

--- server/models/Accounts.js
+++
@@ -1,8 +0,0 @@
-/*import mongoose from'mongoose';
-const userSchema = new mongoose.Schema({
-  email: String,
-  user_id: String,
-  password:String,
-  
-});
-export default  mongoose.model("User", userSchema);*/
